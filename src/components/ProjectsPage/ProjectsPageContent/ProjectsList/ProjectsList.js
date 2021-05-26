8552d8757a8ad10b31bab4fd57e6c277b42df494
Mihail Arhipov <maarkhipov@edu.hse.ru>
2021-05-26 12:26:05 +0300

    (я видимо сохранить забыл ProjectsList в предыдущем коммите)

--- src/components/ProjectsPage/ProjectsPageContent/ProjectsList/ProjectsList.js
+++ src/components/ProjectsPage/ProjectsPageContent/ProjectsList/ProjectsList.js
@@ -3,23 +3,7 @@ import {Link} from 'react-router-dom'
 
 import ProjectPreview from './ProjectPreview/ProjectPreview'
 
-const ProjectsList = ({projectsById}) => {
-    // const projectsArray = []
-
-    // Object.values(projectsById).map( project =>  {
-
-    // })
-
-    // for (let projectId in projectsById) {
-    //     const project = projectsById[projectId]
-    //     projectsArray.push({
-    //         id: project.id,
-    //         name: project.name,
-    //         description: project.description,
-    //         tasksNum: project.tasksIds?.length
-    //     })
-    // }
-    
+const ProjectsList = ({projectsById}) => {    
     return Object.values(projectsById).map( (project) => {
         return(
             <Link to={`/projects/${project.id}`} style={{ textDecoration: 'none' }}>
